20d37adae81727fdf0901e6db822793950bef30f
Lucas Kohorst <kohorstlucas@gmail.com>
2019-10-25 11:39:30 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SentenceTree } from './SentenceTree.jsx';
+
+function App() {
+    return ( <
+        SentenceTree / >
+    );
+}
+
+export default App;
---
+++ src/dfs.js
@@ -0,0 +1,71 @@
+// Array of visited nodes
+var visitConstituency = [];
+// Visited Node Counter
+var vnConstituency = 0;
+/**
+ * DFS that traverses the tree
+ * and then adds a name attribute
+ * in the form of "Word (POS)"
+ * to each node
+ * @param v the structure to traverse
+ */
+function dfsConstituency(v) {
+    Object.keys(v).forEach(function(k) {
+        if (!visitConstituency[vnConstituency++]) {
+            // Checking if ROOT
+            if (v[k].pos === "ROOT") {
+                v[k].name = "ROOT";
+            }
+            if (typeof v[k] === "object") {
+                try {
+                    // Getting the Word and POS
+                    var word = v[k].word;
+                    var pos = v[k].pos.toUpperCase();
+                } catch (error) {}
+                // Adding the name attribute
+                v[k].name = word + " (" + pos + ")";
+                dfsConstituency(v[k]);
+            }
+        } else {
+            visitConstituency.splice(vnConstituency, 0, true);
+        }
+    });
+    return v;
+}
+
+// Array of visited nodes
+var visitDependency = [];
+// Visited Node Counter
+var vnDependency = 0;
+/**
+ * DFS that traverses the tree
+ * and then adds a name attribute
+ * in the form of "Word (POS)"
+ * to each node
+ * @param v the structure to traverse
+ */
+function dfsDependency(v) {
+    Object.keys(v).forEach(function(k) {
+        if (!visitDependency[vnDependency++]) {
+            // Checking if ROOT
+            if (v[k].Relationship === "ROOT") {
+                v[k].name = "ROOT";
+            }
+            if (typeof v[k] === "object") {
+                try {
+                    // Getting the Word and POS
+                    var word = v[k].Name;
+                    var pos = v[k].Relationship.toUpperCase();
+                } catch (error) {}
+                // Adding the name attribute
+                v[k].name = word + " (" + pos + ")";
+                dfsDependency(v[k]);
+            }
+        } else {
+            visitDependency.splice(vnDependency, 0, true);
+        }
+    });
+    return v;
+}
+
+export { dfsConstituency, dfsDependency };
